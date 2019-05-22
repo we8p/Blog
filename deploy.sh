27e31a8fa1ab62358b46e686a876159b59cc45aa
we8p <50701105+we8p@users.noreply.github.com>
2019-05-22 14:22:02 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:A-23187/A-23187.github.io.git master
-git push -f https://$GH_TOKEN@github.com/we8p/we8p.github.io.git master
+git push -f https://${GH_TOKEN}@github.com/we8p/we8p.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
