1173e914eacc96a1b62ebcf9d563db29bc511ec3
we8p <50701105+we8p@users.noreply.github.com>
2019-05-22 13:52:15 +0200

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+yarn build
+
+# 进入生成的文件夹
+cd dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A # 等价于 git add --all
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:A-23187/A-23187.github.io.git master
+git push -f https://$GH_TOKEN@github.com/we8p/we8p.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+cd -
